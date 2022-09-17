b5ed890d67cc76fbebc208ffc9fead52438c6c38
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2022-09-17 00:40:05 -0300

    Final Round Mobile

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -13,13 +13,16 @@ import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
 
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
-import { Heading } from '../../components/Heading';
 import { Background } from '../../components/Background';
+import { DuoMatch } from '../../components/DuoMatch';
+import { Heading } from '../../components/Heading';
 
 
 export function Game() {
 
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
 
   const navigation = useNavigation();
   const routes = useRoute();
@@ -27,6 +30,13 @@ export function Game() {
   
   function handleGoBack(){
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.0.10:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord))
+
   }
 
   useEffect(() => {
@@ -72,7 +82,7 @@ export function Game() {
         renderItem={({ item }) => (
           <DuoCard 
           data = {duos[0]}
-          onConnect={() => { }}
+          onConnect={() => getDiscordUser(item.id)}
           />
         )}
         horizontal
@@ -88,7 +98,11 @@ export function Game() {
           
         }}
       />
-      
+      <DuoMatch 
+        visible={discordDuoSelected.length > 0}
+        discord={discordDuoSelected}
+        onClose={() => setDiscordDuoSelected('')}
+      />
     </SafeAreaView>
     </Background>
   );
